94cc316dddb01786a12a9159be2f123845e3a80c
Arpita-rana <arpitarana0204@gmail.com>
2025-07-30 12:44:58 +0530

    final commit

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Backend is up and running!');
+});
+
 app.post('/bfhl', (req, res) => {
     const input = req.body.data || [];
 
